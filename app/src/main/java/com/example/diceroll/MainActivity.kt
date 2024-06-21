00aebb64fa6df99a257668c01a272a2beaf9e852
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-06-21 13:50:40 +0800

    added strings and prev and img 👾

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroll.ui.theme.DiceRollTheme
@@ -19,29 +22,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DiceRollTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DiceImg(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center)
+                )
             }
         }
     }
 }
 
+
+
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DicePrev(){
+    DiceRollTheme {
+        DiceImg()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DiceImg(modifier: Modifier = Modifier){
     DiceRollTheme {
-        Greeting("Android")
+
     }
 }
